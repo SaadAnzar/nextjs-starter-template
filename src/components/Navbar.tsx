5c0449d843b169aafef4089ea0dfdbacf1ded1ee
Md Anzar Ahmad <anzarhps@gmail.com>
2024-06-12 19:34:08 +0530

    add some UI changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,12 @@ import {
   LayoutDashboard,
   LifeBuoy,
   LogOut,
-  Target,
+  Palette,
+  Pizza,
   User,
 } from "lucide-react";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -28,106 +30,90 @@ const Navbar = () => {
 
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="mx-auto flex h-16 w-full items-center justify-between space-x-4 px-4 sm:px-12">
-        <div className="flex gap-6">
-          {pathname === "/" ? (
-            <div className="flex items-center space-x-2">
-              <Target className="size-6" />
-              <span className="inline-block font-bold">Anzar</span>
-            </div>
-          ) : (
-            <Link
-              href="/"
-              className="flex items-center space-x-2 hover:opacity-80"
-            >
-              <Target className="size-6" />
-              <span className="inline-block font-bold">Anzar</span>
-            </Link>
-          )}
-          <nav className="flex gap-6">
-            {pathname === "/home" ? (
-              <p className="text-muted-foreground flex items-center text-sm font-medium">
-                Home
-              </p>
-            ) : (
-              <Link
-                href="/home"
-                className="hover:text-muted-foreground text-foreground flex items-center text-sm font-medium"
-              >
-                Home
-              </Link>
+      <div className="flex h-16 w-full items-center justify-between space-x-4 px-4 sm:px-12">
+        <nav className="flex flex-row items-center space-x-4 sm:space-x-6">
+          <Link
+            href="/"
+            className="inline-flex items-center pr-4 text-xl font-bold sm:pr-8"
+          >
+            <Pizza className="mr-2 size-5" />
+            Anzar
+          </Link>
+
+          <Link
+            href="/home"
+            className={cn(
+              "font-medium hover:opacity-50",
+              pathname === "/home" && "pointer-events-none opacity-60"
             )}
-
-            {pathname === "/about" ? (
-              <p className="text-muted-foreground flex items-center text-sm font-medium">
-                About
-              </p>
-            ) : (
-              <Link
-                href="/about"
-                className="hover:text-muted-foreground text-foreground flex items-center text-sm font-medium"
-              >
-                About
-              </Link>
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className={cn(
+              "font-medium hover:opacity-50",
+              pathname === "/about" && "pointer-events-none opacity-60"
             )}
-          </nav>
-        </div>
+          >
+            About
+          </Link>
+        </nav>
+        <nav>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost">
+                <User className="size-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mr-2 w-56">
+              <DropdownMenuLabel className="truncate">
+                anzarhps@gmail.com
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
 
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="icon" variant="ghost">
-                  <User className="size-5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="mr-2 w-56">
-                <DropdownMenuLabel className="truncate">
-                  anzarhps@gmail.com
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
+              <DropdownMenuLabel className="inline-flex items-center">
+                <Palette className="mr-2 size-4" />
+                <span className="mr-4">Theme</span>
+                <ThemeToggle />
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
 
-                <DropdownMenuLabel>
-                  <ThemeToggle />
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-
-                <DropdownMenuGroup>
-                  <DropdownMenuItem asChild>
-                    <Link href="/dashboard">
-                      <LayoutDashboard className="mr-2 size-4" />
-                      <span>Dashboard</span>
-                    </Link>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-
-                <DropdownMenuGroup>
-                  <DropdownMenuItem asChild>
-                    <Link href="https://github.com/SaadAnzar" target="_blank">
-                      <Github className="mr-2 size-4" />
-                      <span>GitHub</span>
-                    </Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild>
-                    <Link href="mailto:anzarhps@gmail.com" target="_blank">
-                      <LifeBuoy className="mr-2 size-4" />
-                      <span>Support</span>
-                    </Link>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-
+              <DropdownMenuGroup>
                 <DropdownMenuItem asChild>
-                  <Link href="/" className="cursor-pointer">
-                    <LogOut className="mr-2 size-4" />
-                    <span>Log out</span>
+                  <Link href="/dashboard">
+                    <LayoutDashboard className="mr-2 size-4" />
+                    <span>Dashboard</span>
                   </Link>
                 </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </nav>
-        </div>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <Link href="https://github.com/SaadAnzar" target="_blank">
+                    <Github className="mr-2 size-4" />
+                    <span>GitHub</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="mailto:anzarhps@gmail.com" target="_blank">
+                    <LifeBuoy className="mr-2 size-4" />
+                    <span>Support</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem asChild>
+                <Link href="/" className="cursor-pointer">
+                  <LogOut className="mr-2 size-4" />
+                  <span>Log out</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </nav>
       </div>
     </header>
   );
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,21 +1,71 @@
 "use client";
 
-import * as React from "react";
+import { MoonStar, Smartphone, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
+import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex items-center justify-between space-x-2 px-2">
-      <Label htmlFor="theme-mode">Toggle theme</Label>
-      <Switch
-        id="theme-mode"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      />
-    </div>
+    <TooltipProvider delayDuration={150}>
+      <div className="flex h-[33.25px] flex-row rounded-full border">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              onClick={() => setTheme("system")}
+              className={cn(
+                theme === "system" ? "border opacity-100" : "opacity-60",
+                "flex size-8 items-center justify-center rounded-full hover:opacity-100"
+              )}
+            >
+              <Smartphone className="size-4" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Switch to system mode</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              onClick={() => setTheme("light")}
+              className={cn(
+                theme === "light" ? "border opacity-100" : "opacity-60",
+                "flex size-8 items-center justify-center rounded-full hover:opacity-100"
+              )}
+            >
+              <Sun className="size-4" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Switch to light mode</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              onClick={() => setTheme("dark")}
+              className={cn(
+                theme === "dark" ? "border opacity-100" : "opacity-60",
+                "flex size-8 items-center justify-center rounded-full hover:opacity-100"
+              )}
+            >
+              <MoonStar className="size-4" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Switch to dark mode</p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   );
 }
